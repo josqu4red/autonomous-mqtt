9bb2aa5a5871c775a3b3efd6b056a1f49763197a
Jonathan Amiez <jonathan.amiez@gmail.com>
2025-11-22 21:43:21 +0100

    tmp

--- main/desk.c
+++ main/desk.c
@@ -1,85 +1,86 @@
-#include "esp_log.h"
 #include "desk.h"
+#include "esp_log.h"
+#include "uart.h"
 
 static const char *tag = "desk";
 
-static void build_command(uint8_t* buf, button_t button) {
-    buf[0] = SEND_HEADER1;
-    buf[1] = SEND_HEADER1;
-    buf[2] = SEND_HEADER2;
-    buf[3] = button;
-    buf[4] = button;
+static void build_command(uint8_t *buf, button_t button) {
+  buf[0] = SEND_HEADER1;
+  buf[1] = SEND_HEADER1;
+  buf[2] = SEND_HEADER2;
+  buf[3] = button;
+  buf[4] = button;
 }
 
 static void send_command(button_t command) {
-    uint8_t data[WRITE_BUF] = {0};
-    build_command(data, command);
-    uart_write(data, WRITE_BUF);
+  uint8_t data[WRITE_BUF] = {0};
+  build_command(data, command);
+  uart_write(data, WRITE_BUF);
 }
 
 bool valid_position(position_t position) {
-    if ((position >= position_low) && (position <= position_high)) {
-        return true;
-    }
-    return false;
+  if ((position >= position_low) && (position <= position_high)) {
+    return true;
+  }
+  return false;
 }
 
-void go_to_height(position_t desired, position_t* position) {
-    ESP_LOGI(tag, "Moving to height %d", desired);
-    position_t current = *position;
-    bool done = false;
-    if (desired == current) {
-        return;
-    }
+void go_to_height(position_t desired, position_t *position) {
+  ESP_LOGI(tag, "Moving to height %d", desired);
+  position_t current = *position;
+  bool done = false;
+  if (desired == current) {
+    return;
+  }
 
-    button_t direction;
-    if (desired < current) {
-        direction = button_down;
+  button_t direction;
+  if (desired < current) {
+    direction = button_down;
+  } else {
+    direction = button_up;
+  }
+
+  send_command(button_start);
+
+  while (!done) {
+    vTaskDelay(SEND_DELAY);
+    send_command(direction);
+    current = *position;
+    ESP_LOGD(tag, "position: %d", current);
+
+    if (direction == button_up) {
+      ESP_LOGD(tag, "current %d >= desired %d", current, desired);
+      done = (current >= desired - POSITION_THRESHOLD);
     } else {
-        direction = button_up;
+      ESP_LOGD(tag, "current %d <= desired %d", current, desired);
+      done = (current <= desired + POSITION_THRESHOLD);
     }
-
-    send_command(button_start);
-
-    while(!done) {
-        vTaskDelay(SEND_DELAY);
-        send_command(direction);
-        current = *position;
-        ESP_LOGD(tag, "position: %d", current);
-
-        if (direction == button_up) {
-          ESP_LOGD(tag, "current %d >= desired %d", current, desired);
-          done = (current >= desired - position_threshold);
-        } else {
-          ESP_LOGD(tag, "current %d <= desired %d", current, desired);
-          done = (current <= desired + position_threshold);
-        }
-    }
+  }
 }
 
-void go_to_preset(button_t preset, position_t* position) {
-    ESP_LOGI(tag, "Moving to preset %d", preset);
-    button_t button = presets[preset-1];
-    position_t last = *position;
-    bool done = false;
-    int idle = 0;
+void go_to_preset(button_t preset, position_t *position) {
+  ESP_LOGI(tag, "Moving to preset %d", preset);
+  button_t button = presets[preset - 1];
+  position_t last = *position;
+  bool done = false;
+  int idle = 0;
 
-    send_command(button_start);
+  send_command(button_start);
 
-    while(!done) {
-        vTaskDelay(SEND_DELAY);
-        send_command(button);
-        position_t current = *position;
-        ESP_LOGD(tag, "position: %d; idle:%d", current, idle);
+  while (!done) {
+    vTaskDelay(SEND_DELAY);
+    send_command(button);
+    position_t current = *position;
+    ESP_LOGD(tag, "position: %d; idle:%d", current, idle);
 
-        if(last == current) {
-            idle++;
-            if (idle >= idle_threshold) {
-                done = true;
-            }
-        } else {
-            idle = 0;
-            last = current;
-        }
+    if (last == current) {
+      idle++;
+      if (idle >= IDLE_THRESHOLD) {
+        done = true;
+      }
+    } else {
+      idle = 0;
+      last = current;
     }
+  }
 }
--- main/uart.c
+++ main/uart.c
@@ -1,72 +1,88 @@
-#include "esp_log.h"
 #include "uart.h"
+#include "config.h"
+#include "esp_log.h"
 
-static char* tag = "uart";
+static char *tag = "uart";
 static QueueHandle_t uart0_queue;
 
 void uart_init(void) {
-    const uart_config_t uart_config = {
-        .baud_rate = BAUD_RATE,
-        .data_bits = UART_DATA_8_BITS,
-        .parity = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-        .rx_flow_ctrl_thresh = 122,
-        .source_clk = UART_SCLK_DEFAULT,
-    };
+  const uart_config_t uart_config = {
+      .baud_rate = BAUD_RATE,
+      .data_bits = UART_DATA_8_BITS,
+      .parity = UART_PARITY_DISABLE,
+      .stop_bits = UART_STOP_BITS_1,
+      .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+      .rx_flow_ctrl_thresh = 122,
+      .source_clk = UART_SCLK_DEFAULT,
+  };
 
-    ESP_ERROR_CHECK(uart_driver_install(UART_PORT, BUF_SIZE * 2, 0, 20, &uart0_queue, 0));
-    ESP_ERROR_CHECK(uart_param_config(UART_PORT, &uart_config));
-    ESP_ERROR_CHECK(uart_set_pin(UART_PORT, UART_TXD, UART_RXD, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
-    ESP_ERROR_CHECK(uart_set_mode(UART_PORT, UART_MODE_RS485_HALF_DUPLEX));
+  ESP_ERROR_CHECK(
+      uart_driver_install(UART_PORT, BUF_SIZE * 2, 0, 20, &uart0_queue, 0));
+  ESP_ERROR_CHECK(uart_param_config(UART_PORT, &uart_config));
+  ESP_ERROR_CHECK(uart_set_pin(UART_PORT, UART_TXD, UART_RXD,
+                               UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+  ESP_ERROR_CHECK(uart_set_mode(UART_PORT, UART_MODE_RS485_HALF_DUPLEX));
 }
 
-esp_err_t uart_write(uint8_t* data, int len) {
-    uart_write_bytes(UART_PORT, (char*)data, len);
-    return uart_wait_tx_done(UART_PORT, 100);
+esp_err_t uart_write(uint8_t *data, int len) {
+  uart_write_bytes(UART_PORT, (char *)data, len);
+  return uart_wait_tx_done(UART_PORT, 100 / portTICK_PERIOD_MS);
 }
 
 static uint8_t decode_position(uint8_t *buf) {
-    if(buf[0] != recv_hdr1) { return err_position; };
-    if(buf[1] != recv_hdr1) { return err_position; };
-    if(buf[2] != recv_hdr2a && buf[2] != recv_hdr2b) { return err_position; };
-    if(buf[3] != recv_hdr2a && buf[3] != recv_hdr2b) { return err_position; };
-    if(buf[4] != buf[5]) { return err_position; };
-    return buf[4];
+  if (buf[0] != recv_hdr1) {
+    return err_position;
+  };
+  if (buf[1] != recv_hdr1) {
+    return err_position;
+  };
+  if (buf[2] != recv_hdr2a && buf[2] != recv_hdr2b) {
+    return err_position;
+  };
+  if (buf[3] != recv_hdr2a && buf[3] != recv_hdr2b) {
+    return err_position;
+  };
+  if (buf[4] != buf[5]) {
+    return err_position;
+  };
+  return buf[4];
 }
 
 void uart_event_handler(void *data) {
-    uart_event_t event;
-    uint8_t* arg = (uint8_t*) data;
-    uint8_t msg[READ_BUF] = {0};
-    for (;;) {
-        if (xQueueReceive(uart0_queue, (void*)&event, (TickType_t)portMAX_DELAY)) {
-            switch (event.type) {
-            case UART_DATA:
-                uart_read_bytes(UART_PORT, msg, event.size, portMAX_DELAY);
-                ESP_LOGD(tag, "UART_DATA: 0x%X 0x%X 0x%X 0x%X 0x%X 0x%X", msg[0], msg[1], msg[2], msg[3], msg[4], msg[5]);
-                *arg = decode_position(msg);
-                break;
-            case UART_FIFO_OVF:
-                // If fifo overflow happened, you should consider adding flow control for your application.
-                // The ISR has already reset the rx FIFO,
-                // As an example, we directly flush the rx buffer here in order to read more data.
-                ESP_LOGD(tag, "FIFO overflow");
-                uart_flush_input(UART_PORT);
-                xQueueReset(uart0_queue);
-                break;
-            case UART_BUFFER_FULL:
-                // If buffer full happened, you should consider increasing your buffer size
-                // As an example, we directly flush the rx buffer here in order to read more data.
-                ESP_LOGD(tag, "RX buffer full");
-                uart_flush_input(UART_PORT);
-                xQueueReset(uart0_queue);
-                break;
-            default:
-                ESP_LOGD(tag, "uart event type: %d", event.type);
-                break;
-            }
-        }
+  uart_event_t event;
+  uint8_t *arg = (uint8_t *)data;
+  uint8_t msg[READ_BUF] = {0};
+  for (;;) {
+    if (xQueueReceive(uart0_queue, (void *)&event, 100 / portTICK_PERIOD_MS)) {
+      switch (event.type) {
+      case UART_DATA:
+        uart_read_bytes(UART_PORT, msg, event.size, portMAX_DELAY);
+        ESP_LOGD(tag, "UART_DATA: 0x%X 0x%X 0x%X 0x%X 0x%X 0x%X", msg[0],
+                 msg[1], msg[2], msg[3], msg[4], msg[5]);
+        *arg = decode_position(msg);
+        break;
+      case UART_FIFO_OVF:
+        // If fifo overflow happened, you should consider adding flow control
+        // for your application. The ISR has already reset the rx FIFO, As an
+        // example, we directly flush the rx buffer here in order to read more
+        // data.
+        ESP_LOGD(tag, "FIFO overflow");
+        uart_flush_input(UART_PORT);
+        xQueueReset(uart0_queue);
+        break;
+      case UART_BUFFER_FULL:
+        // If buffer full happened, you should consider increasing your buffer
+        // size As an example, we directly flush the rx buffer here in order to
+        // read more data.
+        ESP_LOGD(tag, "RX buffer full");
+        uart_flush_input(UART_PORT);
+        xQueueReset(uart0_queue);
+        break;
+      default:
+        ESP_LOGD(tag, "uart event type: %d", event.type);
+        break;
+      }
     }
-    vTaskDelete(NULL);
+  }
+  vTaskDelete(NULL);
 }
