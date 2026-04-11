0969674fc12d2a6552709b0e5e2c39f97ed24135
Jonathan Amiez <jonathan.amiez@gmail.com>
2026-04-11 22:17:56 +0200

    Use temp nixpkgs-esp-dev fork to fix deps

--- shell.nix
+++ shell.nix
@@ -1,12 +1,22 @@
 let
   nixpkgs-esp-dev = builtins.fetchGit {
-    url = "https://github.com/mirrexagon/nixpkgs-esp-dev.git";
-    # Optionally pin to a specific commit of `nixpkgs-esp-dev`.
-    # rev = "<commit hash>";
+    url = "https://github.com/josqu4red/nixpkgs-esp-dev";
   };
 
-  pkgs = import <nixpkgs> { overlays = [ (import "${nixpkgs-esp-dev}/overlay.nix") ]; };
-in pkgs.mkShell {
+  pkgs = import <nixpkgs> {
+    overlays = [ (import "${nixpkgs-esp-dev}/overlay.nix") ];
+
+    # The Python library ecdsa is marked as insecure, but we need it for esptool.
+    # See https://github.com/mirrexagon/nixpkgs-esp-dev/issues/109
+    config.permittedInsecurePackages = [
+      "python3.13-ecdsa-0.19.1"
+    ];
+  };
+in
+pkgs.mkShell {
   name = "autonomous-mqtt";
-  buildInputs = with pkgs; [ esp-idf-full ];
+  buildInputs = with pkgs; [
+    esp-idf-full
+    clang-tools
+  ];
 }
